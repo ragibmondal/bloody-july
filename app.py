f627150f2a23e64e7951203f2d61d4610997d9a8
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-07-30 02:54:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def add_red_cloth(image):
     
     for (x, y, w, h) in faces:
         # Define regions for eyes and mouth with reduced size
-        eye_region_height = int(h / 4)
+        eye_region_height = int(h / 5)
         mouth_region_height = int(h / 6)
         
         # Cover eyes
